0a0be76e1565d85476864216212ae6eb7b7a6872
Srija <talamarlasrija@gmail.com>
2024-12-20 01:33:27 +0530

    fetched products and displayed

--- scripts.js
+++ scripts.js
@@ -21,3 +21,80 @@ function toggleMenu() {
         navBar.classList.remove("active");
     }
 }
+
+let skip = 0;
+const limit = 20;
+
+function loadProducts() {
+    fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data.products.length);
+            if (data.products.length == 0) {
+                //no more products
+                window.alert("no more products avaailable");
+            }
+            displayProducts(data.products);
+            skip += limit;
+        });
+}
+
+function displayProducts(products) {
+    products.forEach(product => {
+        const productCard = document.createElement('div');
+
+        productCard.classList.add('product-card');
+
+        productCard.innerHTML = `
+            <img src="${product.thumbnail}" alt="${product.title}" class="product-thumbnail"/>
+            <div class="product-content">
+                <h2 class="product-title">
+                    ${product.title}
+                </h2>
+                <p class="product-description">
+                    ${product.description}
+                </p>
+                <p class="product-price">
+                    Price: $${product.price}
+                </p>
+                <p class="product-stock">
+                    Stock: ${product.stock}
+                </p>
+                <p class="product-rating">
+                    Rating: ${product.rating}/5
+                </p>
+                <button class="product-add-to-cart">
+                    Add to Cart
+                </button>
+            </div>
+        `;
+
+        document.querySelector('.product-container').appendChild(productCard);
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', loadProducts);
+
+
+
+// fetch('https://dummyjson.com/products?limit=200&skip=0')
+//     .then(res => res.json())
+//     .then(data => {
+//         const products = data.products;
+//         console.log(products);
+
+//         products.forEach(product => {
+//             const productCard = document.createElement('div');
+//             productCard.classList.add('product-card');
+
+//             productCard.innerHTML = `
+//             <h2 class = "product-title">${product.title}</h3>
+//         `;
+
+//             document.querySelector('.product-container').appendChild(productCard);
+//         });
+//     })
+//     .catch(err => console.error('Error fetching data', err));
+
+document.getElementById('load-btn').addEventListener("click", loadProducts);
